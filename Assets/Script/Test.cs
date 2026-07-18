dbd57ac8571af4962a4c645fb24c47f092c7ed2f
valentia26 <meam7462@gmail.com>
2026-07-18 10:13:58 +0700

    New test, Deleat framer

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private int n = 1;
+    private float timer = 0f; //นาฬิกาจับเวลา เช่น 1 วิผ่านไป รัวลูปกี่ครั้ง
+
+    void Awake()
+    {
+        Debug.Log("Awake");   
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;    //+= เพิ่มขึ้นเรื่อยๆ
+        n++;
+
+        if (timer >= 1f)
+        {
+            Debug.Log(n);
+            timer = 0f;
+        }
+    }
+}
